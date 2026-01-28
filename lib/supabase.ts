23ebca2f3c1041fa12b1bbad65d7e51734ceb4c9
VochanecA <126856875+VochanecA@users.noreply.github.com>
2026-01-28 11:49:45 +0100

    Supabase u env

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,6 @@
-
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://rmqewezmkomhtmvkkmao.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJtcWV3ZXpta29taHRtdmtrbWFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk1ODk3MDMsImV4cCI6MjA4NTE2NTcwM30.FQ--XG2LtJLvPCf8jyVAQuyFlIb1SCFiW3w1m9sPR_k';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
